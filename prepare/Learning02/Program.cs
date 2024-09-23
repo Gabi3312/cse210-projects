7e4fbeaf810a53731ba0b73027730451d2bb2671
Gabi3312 <gabioros251@gmail.com>
2024-09-23 18:06:15 -0300

    cse210

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+       job1 = new Job();
+       job1._jobTitle = "Software Development";
+       job1._company = "Spotify";
+       job1._startYear = 2023;
+       job1._endYear = 2024;
+
+
+       
+       job job2 = new Job();
+       job2._jobTitle = "Administrative Assistant";
+       job2._company = "Google";
+       job2._startYear = 2022;
+       job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Gabriel Oros";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
